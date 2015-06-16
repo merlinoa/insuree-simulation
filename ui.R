70768fd3d19cd3eaa71e8057009329fecf67dc6e
merlinoa <merlinoa88@gmail.com>
2015-06-16 14:45:52 -0400

    expanding and clarifying assumptions

--- ui.R
+++ ui.R
@@ -74,16 +74,26 @@ body <- dashboardBody(
     tabItem(tabName = "assumptions",
       fluidRow(
         box(width = 12, 
+          h2("Mortality"),
           p("Mortality is simulated in accordance with the ", 
             a(href = "http://www.ssa.gov/oact/STATS/table4c6.html", 
             "Official US Social Secuity Actuarial Table"),".  The
             probability of death at future time periods depends on
-            the age and gender of each individual insuree.  The future life of each
-            insuree is simulated 5,000 times.  The reserve confidence levels are
-            established for all insurees grouped together.  The ", code("insuree"), " 
-            R package is used to run the simulation.  To learn more 
-            about the ", code("insuree"), "package see ", 
-            a(href = "https://github.com/merlinoa/insuree", "the GitHub repository.")
+            the age and gender of each individual insuree.  Curtate age is used for
+            the insueer's age (i.e. the age is rounded down to the nearest whole year) (e.g 
+            if the insurees is 50 and 3 months, 50 will be used for that insuree's age.)"),
+          h2("Benefit / Policy"),
+          p("The benefit payment for each policy is paid if the insuree dies
+            within the term specified by the insuree's policy.  The insuree can 
+            choose to have a deferral period before the term period begins.  If the
+            insuree dies during the deferral period, no beath benefit will be paid.
+            All death benefits are paid at the end of the year of death.  For simplification
+            all policies are assume to be effective today"),
+          h3("Simulation General"),
+          p("The future life of each insuree is simulated 5,000 times.  The reserve confidence 
+            levels are established for all insurees grouped together.  The ", code("insuree"), " 
+            R package is used to run the simulation.  To learn more about the ", code("insuree"), 
+            "package see ", a(href = "https://github.com/merlinoa/insuree", "the GitHub repository.")
           )
         )
       )
